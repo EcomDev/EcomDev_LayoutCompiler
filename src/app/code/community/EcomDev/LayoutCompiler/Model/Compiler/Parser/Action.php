261aee424bff8cdd669fe34fcd9fcdb719a898e5
Ivan Chepurnyi <ivan.chepurnyi@gmail.com>
2015-10-02 21:11:07 +0200

    Fix missing line

--- src/app/code/community/EcomDev/LayoutCompiler/Model/Compiler/Parser/Action.php
+++ src/app/code/community/EcomDev/LayoutCompiler/Model/Compiler/Parser/Action.php
@@ -47,6 +47,9 @@ class EcomDev_LayoutCompiler_Model_Compiler_Parser_Action
 
         $methodArgs = array();
         
+        $args = (array)$element->children();
+        unset($args['@attributes']);
+        
         foreach ($args as $argKey => $argument) {
             if ($argument instanceof SimpleXMLElement) {
                 $methodArgs[$argKey] = $this->parseArgument($argument);
